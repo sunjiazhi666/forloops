5e26c74efc5bd8a6257124e862eda92943eb905c
jiazhi.sun <jiazhi.sun@mu-c1-16.mullen.pvt.k12.co.us>
2018-08-27 08:59:52 -0600

    forloops

--- For Loops/src/forloops.java
+++ For Loops/src/forloops.java
@@ -4,10 +4,36 @@ public class forloops
 
 		public static void main(String[] args)
 			{
-				for (int number = 1, 1 < 5, 1 ++){
-					System.out.println( number );
+
+				challenge1();
+				challenge2();
+				challenge3();
+				challenge4();
+			}
+
+		private static void challenge1()
+			{
+				for(int i = 1; i <= 5; i ++){
+					System.out.println(i);
 				}
 
 			}
 
+		private static void challenge2()
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+		private static Object challenge3()
+			{
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+		private static void challenge4()
+			{
+				// TODO Auto-generated method stub
+
+			}
 	}
